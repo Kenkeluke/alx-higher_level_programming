3eba739e6166c50c3c20d6f3e3ab4aedf4ed5f0d
Kenkeluke <halimahogunbiyi@gmail.com>
2022-12-03 01:51:11 -0800

    Still on modules

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length < 3) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
+const arg = process.argv;
+if (arg.length < 3) { console.log('No argument'); } else if (arg.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
