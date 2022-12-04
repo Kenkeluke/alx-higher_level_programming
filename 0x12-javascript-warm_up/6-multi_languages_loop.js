d5a81c923d85d11ddc6232f780de86d08a760ac2
Kenkeluke <halimahogunbiyi@gmail.com>
2022-12-04 11:55:28 -0800

    Printing language positivity

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const languagePositiveArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < 3) {
+  console.log(languagePositiveArray[i]);
+  i++;
+}
