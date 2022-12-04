f7ddc34c9a40b42387784645844beca62b5bb512
Kenkeluke <halimahogunbiyi@gmail.com>
2022-12-04 12:29:51 -0800

    Number of occurence

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const arg = process.argv;
+const positiveC = 'C is fun';
+const numberOfOcurrence = parseInt(arg[2], 10);
+if (isNaN(numberOfOcurrence)) { console.log('Missing number of occurrences'); } else {
+  let i = 0;
+  while (i < numberOfOcurrence) {
+    console.log(positiveC);
+    i++;
+  }
+}
