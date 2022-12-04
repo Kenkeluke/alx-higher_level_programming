9a35b135bc573b4868195037571ecc2b92cb7c5d
Kenkeluke <halimahogunbiyi@gmail.com>
2022-12-04 13:54:59 -0800

    Printing a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,13 +2,12 @@
 const arg = process.argv;
 const squareRoot = parseInt(arg[2], 10);
 if (isNaN(squareRoot)) { console.log('Missing size'); } else {
-  let square ='';
+  let square = '';
   let i = 0;
   while (i < squareRoot) {
-     for (let j = 0; j < squareRoot; j++) { square += 'X'; }
-     i++
-     if (i < squareRoot) square +='\n';
+    for (let j = 0; j < squareRoot; j++) { square += 'X'; }
+    i++;
+    if (i < squareRoot) square += '\n';
   }
   console.log(square);
-
 }
